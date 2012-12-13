4638242a0d92e988e28e805f89a6c88bc7dab696
Sandeep Mistry <sandeep.mistry@gmail.com>
2012-12-12 20:26:38 -0500

    - re-developed more TDD style

---
+++ src/blink1.js
@@ -0,0 +1,163 @@
+var HID = require('HID');
+
+var VENDOR_ID = 0x27B8;
+var PRODUCT_ID = 0x01ED;
+
+var REPORT_ID = 1;
+var REPORT_LENGTH = 9;
+
+var _blink1HIDdevices = function() {
+  return HID.devices(VENDOR_ID, PRODUCT_ID);
+};
+
+var devices = function() {
+  var serialNumbers = [];
+
+  _blink1HIDdevices().forEach(function(device) {
+    serialNumbers.push(device.serialNumber);
+  });
+
+  return serialNumbers;
+};
+
+function Blink1(serialNumber) {
+  var blink1HIDdevices = _blink1HIDdevices();
+
+  if (blink1HIDdevices.length === 0) {
+    throw new Error('No blink(1)\'s could be found');
+  }
+
+  var blink1HIDdevicePath = null;
+
+  if (serialNumber === undefined) {
+    serialNumber =  blink1HIDdevices[0].serialNumber;
+  }
+
+  blink1HIDdevices.some(function(blink1HIDdevice) {
+    if (serialNumber === blink1HIDdevice.serialNumber) {
+      blink1HIDdevicePath = blink1HIDdevice.path;
+    }
+
+    return (blink1HIDdevicePath !== null);
+  });
+
+  if (blink1HIDdevicePath === null) {
+    throw new Error('No blink(1)\'s with serial number ' + serialNumber + ' could be found');
+  }
+
+  this.serialNumber = serialNumber;
+  this.hidDevice = new HID.HID(blink1HIDdevicePath);
+}
+
+Blink1.prototype._sendCommand = function(/* command [, args ...]*/) {
+  var featureReport = [REPORT_ID, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  featureReport[1] = arguments[0].charCodeAt(0);
+
+  for (var i = 1; i < arguments.length; i++) {
+    featureReport[i + 1] = arguments[i];
+  }
+
+  this.hidDevice.sendFeatureReport(featureReport);
+};
+
+Blink1.prototype._readResponse = function(callback) {
+  callback(this.hidDevice.getFeatureReport(REPORT_ID, REPORT_LENGTH));
+};
+
+Blink1.prototype.version = function(callback) {
+  this._sendCommand('v');
+
+  this._readResponse(function(response) {
+    var version = String.fromCharCode(response[3]) + '.' + String.fromCharCode(response[4]);
+
+    if(callback) {
+      callback(version);
+    }
+  });
+};
+
+Blink1.prototype.eeRead = function(address, callback) {
+  this._sendCommand('e', address);
+
+  this._readResponse(function(response) {
+    var value = response[3];
+
+    if(callback) {
+      callback(value);
+    }
+  });
+};
+
+Blink1.prototype.eeWrite = function(address, value, callback) {
+  this._sendCommand('E', address, value);
+
+  if(callback) {
+    callback();
+  }
+};
+
+Blink1.prototype.fadeToRGB = function(fadeMillis, r, g, b, callback) {
+  var dms = fadeMillis / 10;
+  this._sendCommand('c', r, g, b, dms >> 8, dms % 0xff);
+
+  if(callback) {
+    setTimeout(callback, fadeMillis);
+  }
+};
+//
+Blink1.prototype.setRGB = function(r, g, b, callback) {
+  this._sendCommand('n', r, g, b);
+
+  if (callback) {
+    callback();
+  }
+};
+
+Blink1.prototype.serverDown = function(on, millis, callback) {
+  var dms = millis / 10;
+
+  this._sendCommand('D', on, dms >> 8, dms % 0xff);
+
+  if (callback) {
+    setTimeout(callback, millis);
+  }
+};
+
+Blink1.prototype.play = function(play, position, callback) {
+  this._sendCommand('p', play, position);
+
+  if (callback) {
+    callback();
+  }
+};
+
+Blink1.prototype.writePatternLine = function(fadeMillis, r, g, b, position, callback) {
+  var dms = fadeMillis / 10;
+
+  this._sendCommand('P', r, g, b, dms >> 8, dms % 0xff, position, 0);
+
+  if (callback) {
+    callback();
+  }
+};
+
+Blink1.prototype.readPatternLine = function(position, callback) {
+  this._sendCommand('R', 0, 0, 0, 0, 0, position, 0);
+
+  this._readResponse(function(response) {
+    var value = {
+      r: response[2],
+      g: response[3],
+      b: response[4],
+      fadeMillis: ((response[5] << 8) + (response[6] & 0xff)) * 10
+    };
+
+    if (callback) {
+      callback(value);
+    }
+  });
+};
+
+exports.Blink1 = Blink1;
+exports.devices = devices;
---
+++ test/unit.js
@@ -0,0 +1,383 @@
+
+var should = require('should');
+var mockery = require('mockery');
+
+describe('blink(1)', function() {
+  var BLINK1_SRC_PATH = '../src/blink1';
+
+  var VENDOR_ID = 0x27B8;
+  var PRODUCT_ID = 0x01ED;
+
+  var FEATURE_REPORT_ID = 1;
+  var FEATURE_REPORT_LENGTH = 9;
+
+  var MOCK_HID_DEVICE_1_SERIAL_NUMBER = '1A001407';
+  var MOCK_HID_DEVICE_2_SERIAL_NUMBER = '1A001408';
+  var MOCK_HID_DEVICE_1_PATH = 'path_1A001407';
+  var MOCK_HID_DEVICE_2_PATH = 'path_1A001408';
+
+  var MOCK_HID_DEVICE_1 = {
+    serialNumber: MOCK_HID_DEVICE_1_SERIAL_NUMBER,
+    path: MOCK_HID_DEVICE_1_PATH
+  };
+
+  var MOCK_HID_DEVICE_2 = {
+    serialNumber: MOCK_HID_DEVICE_2_SERIAL_NUMBER,
+    path: MOCK_HID_DEVICE_2_PATH
+  };
+
+  var Blink1;
+  var mockHIDdevices;
+  var sentFeatureReport;
+  var recvFeatureReport;
+
+  var mockHIDdevice = {
+    sendFeatureReport: function(featureReport) {
+      sentFeatureReport = featureReport;
+    },
+
+    getFeatureReport: function(id, length) {
+      return ((id === FEATURE_REPORT_ID ) && (length === FEATURE_REPORT_LENGTH)) ? recvFeatureReport : null;
+    }
+  };
+
+  var mockHID = {
+    devices: function(vendorId, productId) {
+      return ((vendorId === VENDOR_ID) && (productId === PRODUCT_ID)) ? mockHIDdevices : null;
+    },
+
+    HID: function(path) {
+      return (path === MOCK_HID_DEVICE_1_PATH) ? mockHIDdevice : null;
+    }
+  };
+
+  beforeEach(function() {
+    mockery.registerAllowable(BLINK1_SRC_PATH);
+    mockery.enable();
+
+    mockery.registerMock('HID', mockHID);
+
+    Blink1 = require(BLINK1_SRC_PATH);
+  });
+
+  afterEach(function() {
+    mockery.deregisterAllowable(BLINK1_SRC_PATH);
+    mockery.disable();
+
+    mockery.deregisterMock('HID');
+
+    Blink1 = null;
+    mockHIDdevices = null;
+
+    recvFeatureReport = null;
+    sentFeatureReport = null;
+  });
+
+  describe('#devices', function() {
+
+    it('should return no serial numbers when there are no blink(1) HID devices', function() {
+      mockHIDdevices = [];
+
+      Blink1.devices().should.have.length(0);
+    });
+
+    it('should return two serial numbers when there are two blink(1) HID devices', function() {
+      mockHIDdevices = [MOCK_HID_DEVICE_1, MOCK_HID_DEVICE_2];
+
+      Blink1.devices().should.eql([MOCK_HID_DEVICE_1_SERIAL_NUMBER, MOCK_HID_DEVICE_2_SERIAL_NUMBER]);
+    });
+  });
+
+  describe('#Blink1', function() {
+
+    it('should throw an error when there are no blink(1) HID devices', function() {
+      mockHIDdevices = [];
+
+      (function(){
+        new Blink1.Blink1();
+      }).should.throwError('No blink(1)\'s could be found');
+    });
+
+    it('should not throw an error when there are blink(1) HID devices', function() {
+      mockHIDdevices = [MOCK_HID_DEVICE_1];
+
+      new Blink1.Blink1();
+    });
+
+    it('should throw an error when there are no blink(1) HID devices with the supplied serial number', function() {
+      mockHIDdevices = [MOCK_HID_DEVICE_1];
+
+      (function(){
+        new Blink1.Blink1(MOCK_HID_DEVICE_2_SERIAL_NUMBER);
+      }).should.throwError('No blink(1)\'s with serial number ' + MOCK_HID_DEVICE_2_SERIAL_NUMBER + ' could be found');
+    });
+
+    it('should not throw an error when there are blink(1) HID devices with the supplied serial number', function() {
+      mockHIDdevices = [MOCK_HID_DEVICE_1];
+
+      new Blink1.Blink1(MOCK_HID_DEVICE_1_SERIAL_NUMBER);
+    });
+
+    it('should store correct serial number', function() {
+      mockHIDdevices = [MOCK_HID_DEVICE_1];
+
+      var blink1 = new Blink1.Blink1(MOCK_HID_DEVICE_1_SERIAL_NUMBER);
+      blink1.serialNumber.should.eql(MOCK_HID_DEVICE_1_SERIAL_NUMBER);
+    });
+
+    it('should select first blink(1) HID device when no serial number is supplied', function() {
+      mockHIDdevices = [MOCK_HID_DEVICE_1, MOCK_HID_DEVICE_2];
+
+      var blink1 = new Blink1.Blink1();
+      blink1.serialNumber.should.eql(MOCK_HID_DEVICE_1_SERIAL_NUMBER);
+    });
+
+    it('should open correct HID device path', function() {
+      mockHIDdevices = [MOCK_HID_DEVICE_1, MOCK_HID_DEVICE_2];
+
+      var blink1 = new Blink1.Blink1(MOCK_HID_DEVICE_1_SERIAL_NUMBER);
+      blink1.serialNumber.should.eql(MOCK_HID_DEVICE_1_SERIAL_NUMBER);
+      blink1.hidDevice.should.equal(mockHIDdevice);
+    });
+  });
+
+  var blink1;
+
+  var setupBlink1 = function() {
+    mockHIDdevices = [MOCK_HID_DEVICE_1, MOCK_HID_DEVICE_2];
+
+    blink1 = new Blink1.Blink1();
+  };
+
+  var teardownBlink1 = function() {
+    blink1 = null;
+  };
+
+  describe('#Blink1.version', function() {
+
+    beforeEach(function() {
+      setupBlink1();
+
+      recvFeatureReport = [FEATURE_REPORT_ID, 0x76, 0, 0x31, 0x30, 0, 0, 0, 0];
+    });
+    afterEach(teardownBlink1);
+
+    it('should send version feature report', function() {
+      blink1.version();
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x76, 0, 0, 0, 0, 0, 0, 0]);
+    });
+
+    it('should call back with version', function(done) {
+      blink1.version(function(version) {
+        done();
+      });
+    });
+
+    it('should call back with correct version', function(done) {
+
+      blink1.version(function(version) {
+        version.should.eql('1.0');
+        done();
+      });
+    });
+  });
+
+  describe('#Blink1.eeRead', function() {
+
+    var ADDRESS = 1;
+    var VALUE = 5;
+
+    beforeEach(function() {
+      setupBlink1();
+
+      recvFeatureReport = [FEATURE_REPORT_ID, 0x65, 0, VALUE, 0, 0, 0, 0, 0];
+    });
+    afterEach(teardownBlink1);
+
+    it('should send eeread feature report', function() {
+      blink1.eeRead(ADDRESS);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x65, 1, 0, 0, 0, 0, 0, 0]);
+    });
+
+    it('should call back with value', function(done) {
+      blink1.eeRead(ADDRESS, function(value) {
+        done();
+      });
+    });
+
+    it('should call back with correct value', function(done) {
+
+      blink1.eeRead(ADDRESS, function(value) {
+        value.should.eql(VALUE);
+        done();
+      });
+    });
+  });
+
+  describe('#Blink1.eeWrite', function() {
+
+    var ADDRESS = 1;
+    var VALUE = 5;
+
+    beforeEach(setupBlink1);
+    afterEach(teardownBlink1);
+
+    it('should send eewrite feature report', function() {
+      blink1.eeWrite(ADDRESS, VALUE);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x45, ADDRESS, VALUE, 0, 0, 0, 0, 0]);
+    });
+
+    it('should call back', function(done) {
+      blink1.eeWrite(ADDRESS, VALUE, done);
+    });
+  });
+
+  describe('#Blink1.fadeToRGB', function() {
+    var FADE_MILLIS = 10;
+    var R = 10;
+    var G = 20;
+    var B = 40;
+
+    beforeEach(setupBlink1);
+    afterEach(teardownBlink1);
+
+    it('should send fadetorgb feature report', function() {
+      blink1.fadeToRGB(FADE_MILLIS, R, G, B);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x63, R, G, B, (FADE_MILLIS / 10) >> 8, (FADE_MILLIS / 10) % 0xff, 0, 0]);
+    });
+
+    it('should call back', function(done) {
+      blink1.fadeToRGB(FADE_MILLIS, R, G, B, done);
+    });
+  });
+
+  describe('#Blink1.setRGB', function() {
+    var R = 10;
+    var G = 20;
+    var B = 40;
+
+    beforeEach(setupBlink1);
+    afterEach(teardownBlink1);
+
+    it('should send setrgb feature report', function() {
+      blink1.setRGB(R, G, B);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x6e, R, G, B, 0, 0, 0, 0]);
+    });
+
+    it('should call back', function(done) {
+      blink1.setRGB(R, G, B, done);
+    });
+  });
+
+  describe('#Blink1.serverDown', function() {
+    var MILLIS = 10;
+
+    beforeEach(setupBlink1);
+    afterEach(teardownBlink1);
+
+    it('should send serverdown on feature report', function() {
+      blink1.serverDown(1, MILLIS);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x44, 1, (MILLIS / 10) >> 8, (MILLIS / 10) % 0xff, 0, 0, 0, 0]);
+    });
+
+    it('should send serverdown off feature report', function() {
+      blink1.serverDown(0, 0);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x44, 0, 0, 0, 0, 0, 0, 0]);
+    });
+
+    it('should call back', function(done) {
+      blink1.serverDown(1, 0, done);
+    });
+  });
+
+  describe('#Blink1.play', function() {
+    var POSITION = 5;
+
+    beforeEach(setupBlink1);
+    afterEach(teardownBlink1);
+
+    it('should send play on feature report', function() {
+      blink1.play(1, POSITION);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x70, 1, POSITION, 0, 0, 0, 0, 0]);
+    });
+
+    it('should send play off feature report', function() {
+      blink1.play(0, 0);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x70, 0, 0, 0, 0, 0, 0, 0]);
+    });
+
+    it('should call back', function(done) {
+      blink1.play(1, 0, done);
+    });
+  });
+
+  describe('#Blink1.writePatternLine', function() {
+    var FADE_MILLIS = 10;
+    var R = 10;
+    var G = 20;
+    var B = 40;
+    var POSITION = 5;
+
+    beforeEach(setupBlink1);
+    afterEach(teardownBlink1);
+
+    it('should send writepatternline feature report', function() {
+      blink1.writePatternLine(FADE_MILLIS, R, G, B, POSITION);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x50, R, G, B, (FADE_MILLIS / 10) >> 8, (FADE_MILLIS / 10) % 0xff, POSITION, 0]);
+    });
+
+    it('should call back', function(done) {
+      blink1.writePatternLine(FADE_MILLIS, R, G, B, POSITION, done);
+    });
+  });
+
+  describe('#Blink1.readPatternLine', function() {
+    var POSITION = 5;
+
+    var FADE_MILLIS = 1000;
+    var R = 10;
+    var G = 20;
+    var B = 40;
+
+    beforeEach(function() {
+      setupBlink1();
+
+      recvFeatureReport = [FEATURE_REPORT_ID, 0x52, R, G, B, (FADE_MILLIS / 10) >> 8, (FADE_MILLIS / 10) % 0xff, POSITION, 0];
+    });
+    afterEach(teardownBlink1);
+
+    it('should send readpatternline feature report', function() {
+      blink1.readPatternLine(POSITION);
+
+      sentFeatureReport.should.eql([FEATURE_REPORT_ID, 0x52, 0, 0, 0, 0, 0, POSITION, 0]);
+    });
+
+    it('should call back with value', function(done) {
+      blink1.readPatternLine(POSITION, function(value) {
+        done();
+      });
+    });
+
+    it('should call back with correct value', function(done) {
+
+      blink1.readPatternLine(POSITION, function(value) {
+        value.r.should.eql(R);
+        value.g.should.eql(G);
+        value.b.should.eql(B);
+        value.fadeMillis.should.eql(FADE_MILLIS);
+
+        done();
+      });
+    });
+  });
+});
