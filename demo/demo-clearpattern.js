f9c4bb0b4d49e404a41e1423a0e17114e0b68adf
Tod E. Kurt <tod@todbot.com>
2020-09-20 21:29:59 -0700

    tighten up clearpattern demo

--- demo/demo-clearpattern.js
+++ demo/demo-clearpattern.js
@@ -1,16 +1,11 @@
-var async = require('async');
 
 var Blink1 = require('../blink1');
-
 var blink1 = new Blink1();
-
 console.log('clear pattern');
 // clear out all the RAM entries
 for( i=0; i< 32; i++) {
   blink1.writePatternLine(0, 0,0,0, i);
 }
-
 // save RAM to flash
 blink1.savePattern()
-
 blink1.close()
